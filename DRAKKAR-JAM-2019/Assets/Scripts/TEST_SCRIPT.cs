d25656d993ce6397b59aa7fc93bd69ba226e6d0a
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-04-05 19:06:04 +0200

    Test script de prueba

---
+++ DRAKKAR-JAM-2019/Assets/Scripts/TEST_SCRIPT.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_SCRIPT : MonoBehaviour
+{
+    [Header("Variables de movimiento")]
+    [SerializeField] Vector3 vel = new Vector3();
+    [HideInInspector]public bool canMove = true;
+
+    [Header("Variables de los poderes")]
+    [SerializeField] [Range(0,10)] [Tooltip("Esta variable indica la fuerza del poder del personaje")]float power = 5;
+
+    [Header("Perfil del personaje")]
+    [SerializeField] string name = "";
+    [SerializeField] [TextArea(3,10)]string description = "";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("pulsaste enter");
+        }
+    }
+}
