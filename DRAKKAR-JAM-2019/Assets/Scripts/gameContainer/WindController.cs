9b39946b751640022bc0577454b9f7fda7af8435
Darcor\Zuuhr <zuuhr.contact@gmail.com>
2019-04-07 01:08:46 +0200

    shader se mueve cn viento

--- DRAKKAR-JAM-2019/Assets/Scripts/gameContainer/WindController.cs
+++ DRAKKAR-JAM-2019/Assets/Scripts/gameContainer/WindController.cs
@@ -23,6 +23,11 @@ public class WindController : MonoBehaviour
         StartCoroutine("startWind");
     }
 
+    private void LateUpdate()
+    {
+        water.GetComponentInChildren<MeshRenderer>().material.SetVector("_DirectionXY", new Vector4(windDirection.x/2f, windDirection.y/2f, 0, 0));
+    }
+
     IEnumerator startWind()
     {
         timeForWave = Random.Range(minTimeForWave, maxTimeForWave);
@@ -31,11 +36,12 @@ public class WindController : MonoBehaviour
         yield return new WaitForSeconds(timeForWave);
         duringTimeForWave = Random.Range(minDuringTimeForWave, maxDuringTimeForWave);
         windDirection = new Vector2(Random.Range(-187, 187), Random.Range(-187, 187)).normalized * windPower;
-        MeshRenderer mr = water.GetComponentInChildren<MeshRenderer>();
-        /*foreach(MeshRenderer m in mr)
-        {*/
-            mr.material.SetVector("_DirectionXY", new Vector4(windDirection.x, windDirection.y, 0, 0));
-        //}
+        /*MeshRenderer[] mr = water.GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer m in mr)
+        {
+            m.material.SetVector("_DirectionXY", new Vector4(windDirection.x, windDirection.y, 0, 0));
+        }*/
+
         //water.GetComponent<MeshRenderer>().material.SetVector("_DirectionXY", new Vector4(windDirection.x, windDirection.y, 0, 0));
         print("applying wind of " + windDirection);
         yield return new WaitForSeconds(duringTimeForWave);
