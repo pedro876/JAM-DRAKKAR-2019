b6a877e3100c957e0b92332946e63e7745fa6427
Darcor\Zuuhr <zuuhr.contact@gmail.com>
2019-04-06 13:39:04 +0200

    Arreglo script del agua sobre el missing boat

--- DRAKKAR-JAM-2019/Assets/Scripts/WATER SCRIPTS/WaterMovement.cs
+++ DRAKKAR-JAM-2019/Assets/Scripts/WATER SCRIPTS/WaterMovement.cs
@@ -7,12 +7,10 @@ public class WaterMovement : MonoBehaviour
 
     [SerializeField] MeshCollider[] children;
     [SerializeField] float speed = 60f;
-    ShipMove player1;
     int index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        player1 = GameObject.FindGameObjectWithTag("player1").GetComponent<ShipMove>();
         children = GetComponentsInChildren<MeshCollider>();
         StartCoroutine("changeObject");
     }
@@ -27,7 +25,6 @@ public class WaterMovement : MonoBehaviour
         }
         index++;
         if (index > children.Length - 1) index = 0;
-       // player1.wavePositioning();
         yield return new WaitForSeconds(1f / speed);
         StartCoroutine("changeObject");
     }
