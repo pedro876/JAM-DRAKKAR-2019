9879160fd294c7123afd3749c0814b8bfbd48bef
alejandracc <alejandracasado97@gmail.com>
2019-04-06 23:18:58 +0200

    menú

---
+++ DRAKKAR-JAM-2019/Assets/Scripts/UI SCRIPTS/ClickSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSound : MonoBehaviour
+{
+    public Image playButton;
+    public Image quitButton;
+    public Image controlButton;
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        playButton = GameObject.Find("button_play_active").GetComponent<Image>();
+        quitButton = GameObject.Find("button_quit_active").GetComponent<Image>();
+        controlButton = GameObject.Find("button_control_active").GetComponent<Image>();
+    }
+}
