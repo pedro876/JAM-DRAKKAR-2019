d7b2a7a558aef103c0038201456408d142ab69c6
Pedro Casas Martínez <pedrocasas876@gmail.com>
2019-04-06 06:08:42 +0200

    Versión avanzada del movimiento y del script de movimiento del agua

--- DRAKKAR-JAM-2019/Assets/Scripts/SHIPS/ShipMove.cs
+++ DRAKKAR-JAM-2019/Assets/Scripts/SHIPS/ShipMove.cs
@@ -15,16 +15,19 @@ public class ShipMove : MonoBehaviour
 
 
     [Header("Move variables")]
-    [SerializeField] float forwardAcceleration  = 1f;
-    [SerializeField] float stopAcceleration     = 1f;
+    [SerializeField] float xAcceleration  = 1f;
     [SerializeField] float veerAcceleration     = 1f;
     [SerializeField] float X_AxisMaxSpeed       = 10f;
     [SerializeField] float veerMaxSpeed         = 5f;
     int xInput = 0;
     int veerInput = 0;
+    float xSpeed = 0f;
+    float veerSpeed = 0f;
     bool hitRight = false;
     bool hitLeft = false;
     //[Header("Water Positioning variables")]
+
+    [SerializeField] Transform agua;
 
     //ADJUST VARIABLES
     //Transform[] rayCastOrigins;
@@ -36,33 +39,38 @@ public class ShipMove : MonoBehaviour
     {
         //rayCastOrigins = GetComponentsInChildren<Transform>();
         rb = GetComponent<Rigidbody>();
-        layerMask = LayerMask.GetMask("Water");
+        layerMask = LayerMask.GetMask("WaterSurface");
     }
 
     // Update is called once per frame
     void Update()
     {
         manageInputs();
+        //wavePositioning();
+        //print(X_AxisMaxSpeed);
+    }
 
-        Vector3 correctUp = new Vector3(0f,0f,0f);
-        Vector3 correctPos = new Vector3(0f,0f,0f);
-        int count = 0;
-        /*for(int i = 1; i < rayCastOrigins.Length; i++)
-        {
-            Transform t = rayCastOrigins[i];
-            RaycastHit hit;
-            if(Physics.Raycast(t.position, -transform.up, out hit, 50f, layerMask)){
-                correctUp += hit.normal;
-                correctPos += hit.point;
-                count++;
-            }
-        }*/
-        Debug.DrawRay(transform.position, -transform.up, Color.red);
-        correctUp = correctUp.normalized;
-        correctPos = correctPos / (float)count;
-        transform.position = correctPos;
-        transform.up = correctUp;
+    private void FixedUpdate()
+    {
+        agua.Rotate(new Vector3(0f, 0f, 0.1f));
+        wavePositioning();
+        //FORWARD
+        xSpeed += ((float)xInput - (xSpeed / X_AxisMaxSpeed)) * xAcceleration ;
+        xSpeed = Mathf.Clamp(xSpeed, -X_AxisMaxSpeed, X_AxisMaxSpeed);
+        Vector3 dir = xSpeed * transform.forward.normalized;
+        rb.velocity = dir;
 
+        //SIDES
+        veerSpeed += ((float)veerInput - (veerSpeed / veerMaxSpeed)) * veerAcceleration;
+        veerSpeed = Mathf.Clamp(veerSpeed, -veerMaxSpeed, veerMaxSpeed);
+        //transform.localRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + veerSpeed, transform.rotation.eulerAngles.z);
+        //float veerSpeedAux = Mathf.Clamp(veerSpeed, 0.7f, veerMaxSpeed) * Mathf.Abs(veerInput);
+        rb.angularVelocity = transform.up * veerSpeed;
+        //if (veerInput != 0)
+            //rb.AddTorque(transform.up * veerSpeedAux, ForceMode.Force);
+        //else
+            //rb.AddTorque(transform.up * (-veerSpeedAux * 0.2f), ForceMode.Force);
+        //print(veerSpeed);
     }
 
 
@@ -79,5 +87,26 @@ public class ShipMove : MonoBehaviour
         hitRight = Input.GetKeyDown(hitRightKey);
         hitLeft  = Input.GetKeyDown(hitLeftKey);
     }
-
+    private void wavePositioning()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + transform.up, -transform.up, out hit, 10f, layerMask))
+        {
+            //print("hit");
+            transform.position = hit.point;
+            
+            float angle = Vector3.Angle(hit.normal, transform.up);
+            //print(angle);
+            Vector3 aux = Vector3.Cross(transform.up, hit.normal);
+            transform.Rotate(aux, angle);
+            
+        } /*else if (Physics.Raycast(transform.position + transform.up, -transform.up, out hit, -10f, layerMask)){
+            transform.position = hit.point;
+            float angle = Vector3.Angle(hit.normal, transform.up);
+            print(angle);
+            Vector3 aux = Vector3.Cross(transform.up, hit.normal);
+            transform.Rotate(aux, angle);
+        }*/
+        Debug.DrawRay(transform.position + transform.up, -transform.up, Color.red);
+    }
 }
---
+++ DRAKKAR-JAM-2019/Assets/Scripts/WATER SCRIPTS/WaterMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour
+{
+
+    [SerializeField] Transform[] children;
+    [SerializeField] float speed = 60f;
+    int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //children = GetComponentsInChildren<Transform>();
+        StartCoroutine("changeObject");
+    }
+
+    IEnumerator changeObject()
+    {
+        print("changing to " + index);
+        for(int i = 0; i < children.Length; i++)
+        {
+            if (i == index) children[i].gameObject.SetActive(true);
+            else children[i].gameObject.SetActive(false);
+        }
+        index++;
+        if (index > children.Length - 1) index = 0;
+        yield return new WaitForSeconds(1f / speed);
+        StartCoroutine("changeObject");
+    }
+}
